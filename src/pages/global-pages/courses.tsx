07f2e0bba49edb4bfe355eb4209bcc51b3f793ce
Alisher <alishersodiqov09@gmail.com>
2024-09-12 18:21:38 +0500

    design 80%

--- src/pages/global-pages/courses.tsx
+++ src/pages/global-pages/courses.tsx
@@ -1,6 +1,8 @@
 import Breadcrumb from "@/components/custom/breadcrumb/Breadcrumb.tsx";
 import {CourseCard} from "@/components/custom/cards/course-card.tsx";
 import {courseData} from "@/helpers/constanta.tsx";
+import ShinyButton from "@/components/magicui/shiny-button.tsx";
+import {MdOutlineAddCircle} from "react-icons/md";
 
 const Courses = () => {
     const admin_role = sessionStorage.getItem("admin_roles");
@@ -8,9 +10,34 @@ const Courses = () => {
     return (
         <>
             <Breadcrumb pageName={admin_role === 'ADMIN_QUIZ' ? `Yo'nalishlar` : 'Kurslar'}/>
+
+            {/*==================SEARCH=================*/}
+            <div className={`w-full flex justify-between items-center flex-wrap xl:flex-nowrap gap-5 mt-10 mb-6`}>
+                <ShinyButton text={`Qo'shish`} icon={<MdOutlineAddCircle size={30}/>} className={`bg-darkGreen`}/>
+                <div
+                    className={`w-full lg:max-w-[60%] flex justify-start xl:justify-between items-center flex-wrap md:flex-nowrap gap-5`}>
+                    <input
+                        type={`search`}
+                        className="login__input bg-white border border-lighterGreen text-gray-900 rounded-lg focus:border-darkGreen block w-full p-2.5"
+                        placeholder="Name buyicha qidirish"
+                    />
+                    <input
+                        type={`search`}
+                        className="login__input bg-white border border-lighterGreen text-gray-900 rounded-lg focus:border-darkGreen block w-full p-2.5"
+                        placeholder="................."
+                    />
+                    <input
+                        type={`search`}
+                        className="login__input bg-white border border-lighterGreen text-gray-900 rounded-lg focus:border-darkGreen block w-full p-2.5"
+                        placeholder="................"
+                    />
+                </div>
+            </div>
+
+            {/*=======================BODY CARD======================*/}
             <div className={`grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5`}>
                 {courseData.map((data) => (
-                    <CourseCard title={data.title} imgUrl={data.imgUrl} btnName={data.btnName} desc={data.desc}/>
+                    <CourseCard title={data.title} imgUrl={data.imgUrl} desc={data.desc}/>
                 ))}
             </div>
         </>
--- src/pages/quiz-admin/test.tsx
+++ src/pages/quiz-admin/test.tsx
@@ -1,9 +1,71 @@
 import Breadcrumb from "@/components/custom/breadcrumb/Breadcrumb.tsx";
+import Tables from "@/components/custom/tables/table.tsx";
+import {dashboardTbody, dashboardThead} from "@/helpers/constanta.tsx";
+import ShinyButton from "@/components/magicui/shiny-button.tsx";
+import {MdOutlineAddCircle} from "react-icons/md";
 
 const Tests = () => {
     return (
         <>
-            <Breadcrumb pageName={`Testlar`} />
+            <Breadcrumb pageName={`Testlar`}/>
+
+            {/*=================SEARCH================*/}
+            <div className={`w-full flex justify-between items-center flex-wrap xl:flex-nowrap gap-5 mt-10`}>
+                <ShinyButton text={`Qo'shish`} icon={<MdOutlineAddCircle size={30}/>} className={`bg-darkGreen`}/>
+                <div
+                    className={`w-full lg:max-w-[60%] flex justify-start xl:justify-between items-center flex-wrap md:flex-nowrap gap-5`}>
+                    <input
+                        type={`search`}
+                        className="login__input bg-white border border-lighterGreen text-gray-900 rounded-lg focus:border-darkGreen block w-full p-2.5"
+                        placeholder="Test nomi buyicha qidirish"
+                    />
+                    <input
+                        type={`search`}
+                        className="login__input bg-white border border-lighterGreen text-gray-900 rounded-lg focus:border-darkGreen block w-full p-2.5"
+                        placeholder="................."
+                    />
+                    <input
+                        type={`search`}
+                        className="login__input bg-white border border-lighterGreen text-gray-900 rounded-lg focus:border-darkGreen block w-full p-2.5"
+                        placeholder="................"
+                    />
+                </div>
+            </div>
+
+            {/*======================BODY TABLE===========================*/}
+            <div className={`mt-6`}>
+                <Tables thead={dashboardThead}>
+                    {dashboardTbody.map((sts, idx) => (
+                        <tr key={sts.id} className={`hover:bg-whiteGreen duration-100`}>
+                            <td className="border-b border-[#eee] min-w-[200px] p-5 dark:border-strokedark">
+                                <p className="text-black dark:text-white">
+                                    {idx + 1}
+                                </p>
+                            </td>
+                            <td className="border-b border-[#eee] min-w-[200px] p-5 dark:border-strokedark">
+                                <p className="text-black dark:text-white">
+                                    {sts.thead1}
+                                </p>
+                            </td>
+                            <td className="border-b border-[#eee] min-w-[200px] p-5 dark:border-strokedark">
+                                <p className="text-black dark:text-white">
+                                    {sts.thead2}
+                                </p>
+                            </td>
+                            <td className="border-b border-[#eee] min-w-[200px] p-5 dark:border-strokedark">
+                                <p className="text-black dark:text-white">
+                                    {sts.thead3}
+                                </p>
+                            </td>
+                            <td className="border-b border-[#eee] min-w-[200px] p-5 dark:border-strokedark">
+                                <p className="text-black dark:text-white">
+                                    {sts.thead4}
+                                </p>
+                            </td>
+                        </tr>
+                    ))}
+                </Tables>
+            </div>
         </>
     );
 };
