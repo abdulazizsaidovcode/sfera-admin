e8ed43c2fdf14730ee59a23db83d8faeab327cf4
Alisher <alishersodiqov09@gmail.com>
2024-09-13 10:32:53 +0500

    edit

--- src/components/custom/Header/DropdownUser.tsx
+++ src/components/custom/Header/DropdownUser.tsx
@@ -1,4 +1,6 @@
 import AnimatedShinyText from "@/components/magicui/animated-shiny-text.tsx";
+import avatar from '@/assets/images/avatar.png'
+import BlurFade from "@/components/magicui/blur-fade.tsx";
 
 const DropdownUser = () => {
     const admin_role = sessionStorage.getItem('admin_roles');
@@ -13,21 +15,24 @@ const DropdownUser = () => {
             <div className="flex items-center gap-4">
                 <span className="hidden xsm:flex flex-col text-right">
                     <span className="text-xl font-semibold text-white">
-                        <AnimatedShinyText className={`inline-flex items-center justify-center transition ease-out hover:text-whiten-600 hover:duration-300`}>
+                        <AnimatedShinyText
+                            className={`inline-flex items-center justify-center transition ease-out hover:text-whiten-600 hover:duration-300`}>
                             <span>Sodiqov Alisher</span>
                         </AnimatedShinyText>
                     </span>
                     <span className="text-sm font-normal text-white">
                         <AnimatedShinyText className={`inline-flex items-center justify-center transition ease-out hover:text-whiten-600 hover:duration-300`}>
-                            <span>{admin_role ? roles(admin_role) : 'admin'}</span>
+                            <BlurFade className={`mt-1`}>
+                                <span>{admin_role ? roles(admin_role) : 'admin'}</span>
+                            </BlurFade>
                         </AnimatedShinyText>
                     </span>
                 </span>
                 <span className="h-12 w-12 rounded-full bg-slate-200 object-cover overflow-hidden">
                     <img
-                        src={`https://cdn-icons-png.flaticon.com/512/6676/6676023.png`}
+                        src={avatar}
                         alt="User"
-                        className="w-full h-full"
+                        className="w-full h-full scale-[1.17]"
                     />
                 </span>
             </div>
